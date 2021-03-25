f796351105ddc8788fd3c3ec1d72f40704cdf651
Boyan <boyan.siromahov@hotmail.com>
2021-03-25 15:59:16 -0400

    changed spring version to allow for running through intellij

--- src/main/java/model/ShopRepository.java
+++ src/main/java/model/ShopRepository.java
@@ -2,7 +2,9 @@ package model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(collectionResourceRel = "shopsRes", path="shopsRes")
 public interface ShopRepository extends CrudRepository<Shop, Long> {
-    Shop findById(Long id);
+    Shop findById(long id);
 }
