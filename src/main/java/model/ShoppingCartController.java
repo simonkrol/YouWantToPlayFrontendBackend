efbdb471083c8409897b8994dbee8130910987c0
unknown <liammurph1998@gmail.com>
2021-04-08 21:42:33 -0400

    More work, good work

--- src/main/java/model/ShoppingCartController.java
+++ src/main/java/model/ShoppingCartController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
 
 @Controller
 public class ShoppingCartController {
@@ -34,7 +35,8 @@ public class ShoppingCartController {
     public String addProductToCart(@RequestParam(value = "amount") int amount, @PathVariable("id") long id,
                                    @PathVariable("pid") long pid) {
         Product product = prodRepository.findById(pid);
-        ShoppingCart cart = cartRepository.findById(id);
+        ArrayList<ShoppingCart> carts = cartRepository.findAll();
+        ShoppingCart cart = carts.get(0);
 
         if (product.getInventory() < amount){
             System.out.print("wowlowlwowlwowlwololol");
